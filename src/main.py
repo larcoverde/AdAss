74091ad10eb8639a6374e1f02c02008f167c47c9
Lucas Arcoverde <arcoverde@tutamail.com>
2026-07-16 22:22:02 -0300

    Make extract text from PDF.

--- src/main.py
+++ src/main.py
@@ -5,15 +5,19 @@
 import sys
 import pymupdf
 
-doc = None
-
 def open_file():
     filename = input("Enter file: ")
     doc = pymupdf.open(filename)
-    return
+    return doc
+
+def get_text_from_page(doc):
+    for page in doc:
+        text = page.get_text()
+    return text
 
 def main():
-    open_file()
+    doc = open_file()
+    text = get_text_from_page(doc)
     return
 
 if __name__ == "__main__":
